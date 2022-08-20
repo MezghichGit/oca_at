1a8967daff2d3275300b180a44edb92f6a09380c
moahmed amine mezghich <git@mezghich.com>
2022-08-20 13:30:16 +0100

    les types primitifs en Java

--- src/chapitre1/App.java
+++ src/chapitre1/App.java
@@ -4,6 +4,13 @@ public class App {
 
 	public static void main(String[] args) {
 		
+		//float moy = 12.5F; // F : permet de réserver un espace de float 32 bits au lieu de double 64 bits
+		
+		System.out.println(Integer.MAX_VALUE);  // 32 bits : 2147483647
+		System.out.println(Long.MAX_VALUE);     // 64 bits : 9223372036854775807
+		long x = 922_3_372_036_854L; // L: indique qu'il faut réserver un espace de 64 bits
+		
+		/*
 		int x = 0X12F3E;  //0x ou 0X : nombre dans la base hexadécimale
 		System.out.println("x = "+x);
 		
@@ -11,7 +18,7 @@ public class App {
 		System.out.println("k = "+k);
 		
 		int v = 0b111;  // nombre dans la base binaire : 0b ou 0B
-		System.out.println("v = "+v);
+		System.out.println("v = "+v);*/
 		// Commentaire sur une seule ligne
 		/*
 		System.out.println(Byte.MAX_VALUE);
