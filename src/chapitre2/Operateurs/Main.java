c1485bbe5bd9acbfdc95e6aa357fd825bae97efd
moahmed amine mezghich <git@mezghich.com>
2022-08-21 15:28:09 +0100

    partie operators

---
+++ src/chapitre2/Operateurs/Main.java
@@ -0,0 +1,41 @@
+package chapitre2.Operateurs;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		
+		//L'opérateur ternaire
+		/*
+		int x = 11;
+		int y = 22;
+		
+		int res = (x<y)?1:-1;
+		System.out.println("res ="+ res);
+		boolean test = (x<y)?true:false;
+		System.out.println("test ="+ test);
+		System.out.println((x>y)?true:10);
+		int val = (x>10)?(y>20)?2:-2:1;
+		
+		System.out.println("val = "+val);*/
+		/*byte x = 2;
+		byte y = 4;
+		int z = x+y;*/
+		/*
+		short x = 10;
+		short y = 3;
+		//int z = x/y;
+		System.out.println(((double)x)/y);
+		
+		byte val = (byte)254;
+		System.out.println("val="+val);
+		
+		int y1 = 20;
+		long x1 = y1;*/
+		
+		long x = 10;
+		int y = 2 * ((int) x);
+
+	}
+
+}
