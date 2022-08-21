560811a1bae463c158df2c4db4675634c946c5fe
moahmed amine mezghich <git@mezghich.com>
2022-08-21 11:33:16 +0100

    Garbage Collector - Fin du chapitre

--- src/chapitre1/LeGarbageCollector/Animal.java
+++ src/chapitre1/LeGarbageCollector/Animal.java
@@ -2,4 +2,8 @@ package chapitre1.LeGarbageCollector;
 
 public class Animal {
 
+	public void finalize()
+	{
+		System.out.println("Destruction d'unb objet Animal");
+	}
 }
--- src/chapitre1/LeGarbageCollector/App.java
+++ src/chapitre1/LeGarbageCollector/App.java
@@ -3,7 +3,21 @@ package chapitre1.LeGarbageCollector;
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Animal a1 = new Animal();
+		Animal a2 = new Animal();
+		Animal a3 = new Animal();
+		
+		a1 = a2;  //premier objet perdu
+		a3 = null; // deuxième objet perdu
+	
+		System.gc(); // appel explicite du grabage collector
+		
+		for(int i=0;i<1000;i++)
+		{
+			System.out.println("i = "+i);
+		}
+		
+		System.out.println("Fin du programme...");
 
 	}
 
